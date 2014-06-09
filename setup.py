3e8e76ca7790a8eba7686a323fa5c5bc35d3ccd4
wiadufachen <wiadufachen@gmail.com>
2014-06-09 18:16:55 +0800

    Still 1.3, uploaded to pypi

--- setup.py
+++ setup.py
@@ -1,20 +1,18 @@
-# !/usr/bin/env python3
 from distutils.core import setup
 
-try:
-    import py2exe
-except ImportError:
-    py2exe = None
-    pass
 
 setup(
-    name='mydupfilekiller',
-    description="A Duplicate File Killer",
-    version='1.3',
-    console=[{'script': 'mydupfilekiller.py'}],
-    options={"py2exe": dict(compressed=True,
-                            optimize=2,
-                            bundle_files=1,
-                            includes="ctypes")},
-    zipfile=None
+    name= "mydupfilekiller",
+    packages = ["mydupfilekiller"],
+    description= "A Duplicate File Killer",
+    version= "1.3",
+    author = "Wiadufa Chen",
+    author_email = "wiadufachen@gmail.com",
+    url = "https://github.com/wiadufachen/mydupfilekiller",
+    classifiers = ["Programming Language :: Python",
+                   "Programming Language :: Python :: 3",
+                   "License :: OSI Approved :: Apache Software License",
+                   "Development Status :: 4 - Beta",
+                   "Operating System :: OS Independent",
+                  ]
 )
